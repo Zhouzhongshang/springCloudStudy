6cba8da28f784cbec92d47e77349a7d5b9fa90c5
Your Name <969066788@qq.com>
2020-04-18 20:54:13 +0800

    多线程

--- service-feign/src/main/java/com/zhou/servicefeign/config/ThreadPoolConfig.java
+++ service-feign/src/main/java/com/zhou/servicefeign/config/ThreadPoolConfig.java
@@ -1,10 +1,12 @@
 package com.zhou.servicefeign.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
 
