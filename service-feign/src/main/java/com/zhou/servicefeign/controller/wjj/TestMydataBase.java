0f464f6d339e4087e0405ff20edbcdddc8d7292e
dazhou <969066788@qq.com>
2020-11-23 21:33:44 +0800

    add spring interface transactional method use

--- service-feign/src/main/java/com/zhou/servicefeign/controller/wjj/TestMydataBase.java
+++ service-feign/src/main/java/com/zhou/servicefeign/controller/wjj/TestMydataBase.java
@@ -1,12 +1,16 @@
 package com.zhou.servicefeign.controller.wjj;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.zhou.servicefeign.dao.dao.TestDbDao;
 import com.zhou.servicefeign.pojo.dto.entity.TestDb;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,8 +21,10 @@ import java.util.List;
  * @create: 2020-11-22 20:04
  **/
 @RestController
-public class TestMydataBase {
+@Slf4j
+public class TestMydataBase implements Serializable {
 
+    private static final long serialVersionUID = -8836891662583598196L;
     @Autowired
     private TestDbDao testDbDao;
 
@@ -30,12 +36,23 @@ return testDbDao.queryAll(testDb);
     }
 
     @PostMapping("insert01")
-    public void insert(){
-        TestDb testDb = new TestDb();
-//        testDb.setIsDelete(0);
-        testDb.setCreateTime(LocalDateTime.now());
-        testDb.setUpdateTime(LocalDateTime.now());
-        testDb.setName("zzs");
-        testDbDao.insert(testDb);
+    @Transactional(rollbackFor = Exception.class)
+    public void insert() throws Exception {
+        TestDb testDb01 = new TestDb();
+        testDb01.setIsDelete(0);
+        testDb01.setCreateTime(LocalDateTime.now());
+        testDb01.setUpdateTime(LocalDateTime.now());
+        testDb01.setName("zzs");
+        int insert1 = testDbDao.insert(testDb01);
+
+        TestDb testDb02 = new TestDb();
+        testDb02.setIsDelete(0);
+        testDb02.setCreateTime(LocalDateTime.now());
+        testDb02.setUpdateTime(LocalDateTime.now());
+        testDb02.setName("zzs");
+        int insert = testDbDao.insert(testDb02);
+        if (insert==1){
+            throw new Exception("主动跑出异常");
+        }
     }
 }
