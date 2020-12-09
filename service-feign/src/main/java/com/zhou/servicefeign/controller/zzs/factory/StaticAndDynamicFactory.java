977d4af93dcdce376d25b762537daab84a91cad2
dazhou <969066788@qq.com>
2020-12-09 23:12:44 +0800

    静态工厂和实例（动态）工厂的区别

---
+++ service-feign/src/main/java/com/zhou/servicefeign/controller/zzs/factory/StaticAndDynamicFactory.java
@@ -0,0 +1,48 @@
+package com.zhou.servicefeign.controller.zzs.factory;
+
+/**
+ * @program: sc-f-chapter1
+ * @description: 工厂
+ * @author: zzs
+ * @create: 2020-12-09 22:53
+ **/
+public class StaticAndDynamicFactory {
+
+
+    /**
+     *      Integer instance = Factory.getInstance();
+     *             Factory factory = new Factory();
+     *             Integer integer1 = factory.getInteger1();
+     *
+     *
+     * 静态工厂
+     * <bean class="net.web.ExternalBeanStaticFactory" factory-method="getInstance"
+     * id="okHttpClientStatic"></bean>
+     *
+     *  动态工厂--实例工厂：指定相关方法生成对象、需要更多的内存
+     * <bean class="net.web.ExternalBeanFactory" id="ExternalBeanFactory"/>
+     * <bean class="okhttp3.OkHttpClient" factory-bean="ExternalBeanFactory"
+     * factory-method="getInstance" id="okHttpClient"></bean>
+     */
+
+    private static Integer integer;
+
+        private Integer integer1;
+
+        //静态工厂
+        public static Integer getInstance(){
+            if (null == integer){
+                return 1;
+            }
+            return integer;
+        }
+
+
+        //动态工厂
+        public Integer getInteger1(){
+            if (integer1== null){
+                return 1;
+            }
+            return integer1;
+    }
+}
--- service-feign/src/main/java/com/zhou/servicefeign/controller/zzs/thread/saletickets/TestSaleTickets.java
+++ service-feign/src/main/java/com/zhou/servicefeign/controller/zzs/thread/saletickets/TestSaleTickets.java
@@ -14,13 +14,13 @@ import java.util.concurrent.locks.ReentrantLock;
  **/
 public class TestSaleTickets {
 
-    public static int tickets = 100;
+    public static int tickets = 1000;
 
     public static  ReentrantLock reentrantLock = new ReentrantLock();
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <4 ; i++) {
+        for (int i = 0; i <100 ; i++) {
             new Thread(()->{
                 //获取锁
             while (true) {
